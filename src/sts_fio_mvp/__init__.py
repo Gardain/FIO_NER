08838c4bbf4ddb992275edee6517e7a60441bdc0
Gardain <75211148+Gardain@users.noreply.github.com>
2026-04-23 17:10:42 +0300

    Update __init__.py

--- src/sts_fio_mvp/__init__.py
+++ src/sts_fio_mvp/__init__.py
@@ -1,6 +1,6 @@
-"""OCR + BERT FIO extraction MVP for Russian STS images."""
+"""OCR + NER FIO extraction MVP for Russian STS images."""
 
-from .extractor import DEFAULT_NER_MODEL, DEFAULT_QA_MODEL, ExtractorMode, FioResult
+from .extractor import DEFAULT_NER_MODEL, FioResult
 from .pipeline import StsExtractionResult, extract_fio_from_image
 
 __version__ = "0.1.0"
@@ -8,8 +8,6 @@ __version__ = "0.1.0"
 __all__ = [
     "__version__",
     "DEFAULT_NER_MODEL",
-    "DEFAULT_QA_MODEL",
-    "ExtractorMode",
     "FioResult",
     "StsExtractionResult",
     "extract_fio_from_image",
